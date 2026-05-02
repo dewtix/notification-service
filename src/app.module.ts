27dbd2530513631b070f6345b26c28d326f1ca9b
Kate <kate.dew.next@gmail.com>
2026-05-02 13:20:31 +0200

    add smtp config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
+import configuration from './config/configuration'
+import validationSchema from './config/validation.schema'
 import { RmqModule } from './infrastructure/rmq/rmq.module'
 import { NotificationsModule } from './modules/notifications/notifications.module'
 
@@ -8,7 +10,8 @@ import { NotificationsModule } from './modules/notifications/notifications.modul
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
-			expandVariables: true
+			expandVariables: true,
+			load: [configuration]
 		}),
 		RmqModule,
 		NotificationsModule
--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,8 @@ async function bootstrap() {
 	app.connectMicroservice<MicroserviceOptions>({
 		transport: Transport.RMQ,
 		options: {
-			urls: [config.get('RMQ_URL')],
-			queue: config.get('RMQ_QUEUE'),
+			urls: [config.get('rmq.url')],
+			queue: config.get('rmq.queue'),
 			queueOptions: {
 				durable: true
 			},
